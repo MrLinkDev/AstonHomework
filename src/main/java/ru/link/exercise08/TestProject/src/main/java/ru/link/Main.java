50b97d4e5d24db11d3b98a014d63aef2bfda11a7
Alexander Gorbunov <sasha2000.gorbunov@gmail.com>
2024-06-08 22:11:25 +0700

    AH-8: В develop-ветке добавлен код для вызова конфликта слияния

--- src/main/java/ru/link/exercise08/TestProject/src/main/java/ru/link/Main.java
+++ src/main/java/ru/link/exercise08/TestProject/src/main/java/ru/link/Main.java
@@ -3,5 +3,7 @@ package ru.link;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
+
+        System.out.println("Conflict code in develop");
     }
 }
